3401061452c03089af582c5414d67a5c92520733
laurence burden <laurenceburden@gmail.com>
2024-03-06 20:08:03 -0600

    Added main scene and spinning sprite animation code

---
+++ Singletons/ImageManager.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var _item_images: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	load_item_images()
+	
+# Add the file as a dictionary to the _item_images array
+func add_file_to_list(fn: String, path: String) -> void:
+	var full_path = path + "/" + fn
+	
+	var ii_dict = {
+		"item_name": fn.rstrip(".png"),
+		"item_texture": load(full_path)
+	}
+	
+	_item_images.append(ii_dict)
+
+func load_item_images() -> void:
+	var path: String = "res://assets/glitch"
+	var dir = DirAccess.open(path)
+	
+	if !dir:
+		print("ERROR READING PATH: ", path)
+		return
+		
+	var file_names = dir.get_files()
+	
+	# Keep all .png files, but remove .png.import files
+	for fn in file_names:
+		if ".png" in fn and ".import" not in fn:
+			add_file_to_list(fn, path)
+			
+	print("Loaded:", _item_images.size())
+
+func get_rand_item_image() -> Dictionary:
+	return _item_images.pick_random()
